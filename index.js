f2ca78efee3c14318c082b0d287e38d35382be16
mquandvr <mquan2793@gmail.com>
2024-03-12 13:23:37 +0700

    update port server

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use('/media', express.static(__dirname + '/assets'));
 
 //app.use(express.static('/assets'));
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(80);
+console.log('Listening on port 80');
