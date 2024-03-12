8951999a1d40cf9ef5b7c0ee97ec29c2e55b868d
mquandvr <mquan2793@gmail.com>
2024-03-12 21:47:29 +0700

    add write file meta json

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ var app = express();
 //app.use(express.static(__dirname)); // Current directory is root
 app.use('/media', express.static(__dirname + '/assets'));
 
+app.use('/update-meta', (req, res) => {
+    // res.send('updateMeta');
+        console.log("update Meta")
+    const request = require('request');
+
+    let url = "https://script.googleusercontent.com/macros/echo?user_content_key=6gdThRofZLYQQbwMQ0kMHcitIgSzEsbJxKHaV_7m4vKGke23v_jWEKsdD4HXRyQ7l8T1NSUzKFKM22VrEGwLd-sVCSxNYVI_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnNbSNYUf49WhSblcpEssPibXxunUr0fAUIH7LR13GIFHjaI55AelAfXKuoNYuvi_wB85J8-vKCy4LhKo8EIPsp5H7k9httSgJg&lib=Mo_ff6-GGtnbrTI8W1SVhSA8PhaH1kT4G";
+
+    let options = {json: true};
+
+    request(url, options, (error, resp, body) => {
+        if (error) {
+            console.log(error)
+            return res.send('error');
+        };
+
+        if (!error && res.statusCode == 200) {
+            // do something with JSON, using the 'body' variable
+            console.log("body", body.data);
+            fs.writeFileSync('./src/data/meta.json', JSON.stringify(body.data));
+            res.send('done');
+        };
+    });
+
+});
+
 app.use('/healthz', (req, res) => {
     res.send('healthz');
 });
@@ -20,7 +45,6 @@ app.use('/healthz', (req, res) => {
 app.use('/', (req, res) => {
     res.send('hello world');
 });
-
 //app.use(express.static('/assets'));
 
 app.listen(80);
