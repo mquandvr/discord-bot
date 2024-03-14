8af48f8559209a1191968f000ed1c10d97d0a25d
mquandvr <mquan2793@gmail.com>
2024-03-14 12:34:41 +0700

    fixbug search image name

--- src/commands/Community/gc-hero.js
+++ src/commands/Community/gc-hero.js
@@ -50,8 +50,8 @@ const execute = async (interaction, client) => {
     let siEmbedArr = [];
 
     if (fileNames && fileNames.length > 0) {
-        const equipFileNames = fileNames.filter(file => file.includes("equip"));
-        const siFileNames = fileNames.filter(file => file.includes("si"));
+        const equipFileNames = fileNames.filter(file => file.includes("_equip"));
+        const siFileNames = fileNames.filter(file => file.includes("_si"));
 
         console.log("equipFileNames", equipFileNames);
 
