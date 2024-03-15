fd4dcafffe22e8e9a7a6636e0cc527bc2fd68034
mquandvr <mquan2793@gmail.com>
2024-03-15 21:45:02 +0700

    add write file hero

--- src/utils/files.js
+++ src/utils/files.js
@@ -2,23 +2,32 @@ const fs = require('fs');
 
 const writeData = (filePath, data) => {
     try {
-        fs.writeFileSync(filePath, JSON.stringify(data));
+        fs.stat(filePath, (err, stat) => {
+            if (!err || err.code === 'ENOENT') {
+                fs.writeFileSync(filePath, JSON.stringify(data));
+            } else {
+                console.error("Cant write file");
+            }
+        });
     } catch (e) {
         console.error(e);
     }
 };
-
+ 
 const writeFile = (data) => {
     try {
         console.log("data write", data);
         if (data.meta) {
             writeData('./src/data/meta.json', data.meta);
         }
-        if (data.data.attribute) {
+        if (data.attribute) {
             writeData('./src/data/attribute.json', data.attribute);
         }
-        if (data.data.class) {
+        if (data.class) {
             writeData('./src/data/class.json', data.class);
+        }
+        if (data.hero) {
+            writeData('./src/data/hero.json', data.hero);
         }
     } catch (e) {
         console.error(e);
