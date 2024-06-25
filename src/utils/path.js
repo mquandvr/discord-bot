7c12611f7df3445063cd47cf5ccf9425996a29f0
mquandvr <mquan2793@gmail.com>
2024-06-25 15:16:36 +0700

    update getFileName

--- src/utils/path.js
+++ src/utils/path.js
@@ -3,7 +3,7 @@ import path from "path";
 function getFileName(url) {
     const fileName = path.basename(url);
     const relativePath = path.relative(url, fileName);
-    return relativePath ? relativePath : fileName;
+    return relativePath || fileName;
 }
 
 export { getFileName };
